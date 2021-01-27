20aa0914c87ed3b0b959cf463d516ba772a26de1
Adna Maric <adna.maric@edu.fit.ba>
2021-01-27 17:25:21 +0100

    adding services

---
+++ exploreMostar.WEBAPI/Services/IKorisniciServis.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exploreMostar.WebAPI.Services
+{
+    interface IKorisniciServis
+    {
+    }
+}
--- exploreMostar.WEBAPI/Startup.cs
+++ exploreMostar.WEBAPI/Startup.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using exploreMostar.WebAPI.Database;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -27,6 +29,10 @@ namespace exploreMostar.WEBAPI
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSwaggerGen();
+
+            var connection = @"Server=DESKTOP-HB2VMU2\ADNASQLSERVER;Database=exploreMostar;Trusted_Connection=true;ConnectRetryCount=0";
+            services.AddDbContext<exploreMostarContext>(options => options.UseSqlServer(connection));
+
 
         }
 
