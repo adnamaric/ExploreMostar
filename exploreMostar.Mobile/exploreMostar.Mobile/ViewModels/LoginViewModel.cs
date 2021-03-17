c092f1f7a39625782ab6b7a4d1f97e771f5105ff
Adna Maric <adna.maric@edu.fit.ba>
2021-03-17 20:32:29 +0100

    login improved on mobile application with password hash comparing function

--- exploreMostar.Mobile/exploreMostar.Mobile/ViewModels/LoginViewModel.cs
+++ exploreMostar.Mobile/exploreMostar.Mobile/ViewModels/LoginViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using exploreMostar.Mobile.Views;
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -42,30 +43,77 @@ namespace exploreMostar.Mobile.ViewModels
                 }
             });
         }
+        public static string GenerateHash(string salt, string password)
+        {
+            byte[] src = Convert.FromBase64String(salt);
+            byte[] bytes = Encoding.Unicode.GetBytes(password);
+            byte[] dst = new byte[src.Length + bytes.Length];
+
+            System.Buffer.BlockCopy(src, 0, dst, 0, src.Length);
+            System.Buffer.BlockCopy(bytes, 0, dst, src.Length, bytes.Length);
+
+            HashAlgorithm algorithm = HashAlgorithm.Create("SHA1");
+            byte[] inArray = algorithm.ComputeHash(dst);
+
+            return Convert.ToBase64String(inArray);
+        }
+        protected string hash;
         async Task Login()
         {
             
             IsBusy = true;
             APIService.Username = Username;
             APIService.Password = Password;
+            
             var result = await _service.Get<List<Model.Korisnici>>(null);
-            Model.Korisnici korisnik = null;
-            foreach(var item in result)
+            Model.Korisnici korisnik = new Model.Korisnici();
+            bool pronadjen = false;
+            bool pronadjen2 = false;
+
+
+            foreach (var temp in result)
             {
-                if (item.KorisnickoIme == Username)
-                    korisnik = item;
-            }
-            try
-            {
-                await _service.Get<dynamic>(null);
-               
-                
-                Application.Current.MainPage = new UserPreferenceContentPage();
-            }
-            catch (Exception ex)
-            {
+                if (temp.KorisnickoIme == Username)
+                {
+                    korisnik = temp;
+                    pronadjen = true;
+                    hash = GenerateHash(korisnik.LozinkaSalt, Password);
+                    if (temp.LozinkaHash == hash)
+                    {
+                        pronadjen2 = true;
+                    }
+                }
+
 
             }
+
+            if (pronadjen2 == true && pronadjen == true)
+            {
+                try
+                {
+                    APIService.Username = Username;
+                    APIService.Password = Password;
+
+                    await _service.Get<dynamic>(null);
+                    Application.Current.MainPage = new UserPreferenceContentPage();
+
+
+                }
+                catch (Exception ex)
+                {
+                    //  MessageBox.Show(ex.Message, "Authentifikacija", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+
+                }
+
+            }
+            else
+            {
+                //  MessageBox.Show("Molimo vas unesite ispravno korisničko ime i/ili lozinku", "Pokušajte ponovo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                await Application.Current.MainPage.DisplayAlert("Try again", "Please enter correct username and/or password !", "OK");
+
+            }
+           
         }
     }
 }
